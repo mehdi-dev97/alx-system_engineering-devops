8dabbd704bfb9dffeb39c35c8897ae4feef62ef8
Mehdi Ait Mouh <mehdi.aitmouhD.dev@gmail.com>
2023-11-24 09:52:58 -0800

    Create shchool file in temp dir using puppet.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create school file in /tmp directory
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
